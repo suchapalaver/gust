6293793f0ab8d632eb7334de252d287974539028
“suchapalaver” <jlivesey@gmail.com>
2021-11-13 14:11:12 -0500

    edited error message in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::process;
 
 fn main() {
     if let Err(e) = grusterylist::run() {
-        eprintln!("Problem running application: {}", e);
+        eprintln!("Problem running application:\n{}", e);
         process::exit(1);
     }
 }
