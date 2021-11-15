b3e34503d7aebe8f0a25f100307fcab0627799d9
“suchapalaver” <jlivesey@gmail.com>
2021-11-15 18:35:46 -0500

    added drop() to main.rs to be sure any error destructor is destroyed before calling `exit`

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::process;
 fn main() {
     if let Err(e) = grusterylist::run() {
         eprintln!("Problem running application:\n{}", e);
-        process::exit(1);
+	drop(e); // From the [docs](https://doc.rust-lang.org/std/process/fn.exit.html)\
+	         // If a clean shutdown is needed it is recommended to only call this function at a known point
+	         // where there are no more destructors left to run.
+	process::exit(1);
     }
 }
