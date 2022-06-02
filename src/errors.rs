5ccc9fb9f7543d43d79f808a826a1a7c1a1885be
Joseph Livesey <jlivesey@gmail.com>
2022-06-02 13:47:08 -0400

    fix `Error` type conflict

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
 use serde::de::Error;
+use std::error::Error as StdError;
 
 custom_error::custom_error! {pub ReadError
     DeserializingError{ source: serde_json::Error } = "Invalid JSON file",
-    PathError{ source: Box<dyn Error> } = "Invalid file path",
+    PathError{ source: Box<dyn StdError> } = "Invalid file path",
 }
