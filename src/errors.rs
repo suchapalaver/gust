72cd710cbbae37fe2b1e8c1dd7d2df54c579707f
Joseph Livesey <jlivesey@gmail.com>
2022-06-02 13:37:25 -0400

    change `ModelError` to `ReadError`

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-custom_error::custom_error! {pub ModelError
+custom_error::custom_error! {pub ReadError
     DeserializingError{serde_json::Error} = "Invalid JSON file",
     PathError{Box<dyn Error>} = "Invalid file path",
 }
