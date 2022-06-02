762f3b0b4ed2dbd6167fca8a5c2622005ab39f5c
Joseph Livesey <jlivesey@gmail.com>
2022-06-02 13:41:13 -0400

    add import `serde` deserialization `Error`

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use serde::de::Error;
+
 custom_error::custom_error! {pub ReadError
     DeserializingError{ source: serde_json::Error } = "Invalid JSON file",
     PathError{ source: Box<dyn Error> } = "Invalid file path",
