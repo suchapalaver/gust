2061382ece51c63ac0de42b41bd47dc49665c52a
Joseph Livesey <joseph.livesey@btp.works>
2023-01-01 13:25:45 -0500

    fix: include source in errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,7 @@
 custom_error::custom_error! {pub ReadError
-    DeserializingError{ source: serde_json::Error } = "Invalid JSON file",
+    DeserializingError{ source: serde_json::Error } = "Invalid JSON file {source}",
     ParseInputError = "Invalid input",
-    ReadWriteError{ source: std::io::Error } = "Error reading/writing file",
+    ReadWriteError{ source: std::io::Error } = "Error reading/writing file {source}",
     ItemNotFound = "Item not found",
     LibraryNotFound = "No groceries library found.\nRun grusterylist groceries to create a groceries library",
 }
